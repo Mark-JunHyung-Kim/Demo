18eca4e94a185034c2c28036c88c350eaf9da5c2
Leian <Leian@DESKTOP-9DBMGK5>
2020-05-12 14:08:51 -0700

    It is the main Project

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,27 @@
 
+import java.util.Scanner;	// 항상 이걸 먼저 써줘야 스캐너를 쓸수있음
 public class Hello {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Hello!");
+		/*
+		 System.out.print(myAge + "\n\n");	// println이랑 print는 writeline이랑 write의 차이임.
+		 */
+		int age = 36;
+		String name = "Mark Kim";		
+				
+				
+		System.out.println("Hello! My Age is = " + age + " and Name is = " + name);
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your Name >> ");
+		name = input.nextLine();
+		
+		System.out.println("What is your Age >> ");
+		age = input.nextInt();
+				
+		System.out.println("Hello! My Age is = " + age + " and Name is = " + name);
+		
 	}
 }
